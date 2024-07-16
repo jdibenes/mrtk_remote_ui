3b2a9d13bde7009c30d72821a430f4687c54a5d0
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-07-15 20:50:49 -0400

    cleanup library

--- client/hl2ss_uifm.py
+++ client/hl2ss_uifm.py
@@ -67,11 +67,26 @@ class TextOverflowModes:
     Linked = 6
 
 
+def _pack_params_struct(*args):
+    header = bytearray()
+    offset = hl2ss._SIZEOF.DWORD * len(args)
+    for blob in args:
+        header.extend(struct.pack('<I', offset))
+        offset += len(blob)
+    for blob in args:
+        header.extend(blob)
+    return header
+
+
 #------------------------------------------------------------------------------
 # Commands
 #------------------------------------------------------------------------------
 
 class command_buffer(hl2ss.umq_command_buffer):
+    #--------------------------------------------------------------------------
+    # Remote Unity Scene IO Map
+    #--------------------------------------------------------------------------
+
     def create_primitive(self, type):
         self.add(0, struct.pack('<I', type))
 
@@ -117,27 +132,33 @@ class command_buffer(hl2ss.umq_command_buffer):
     def set_target_mode(self, mode):
         self.add(20, struct.pack('<I', mode))
 
-    # File IO Map -------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    # File IO Map
+    #--------------------------------------------------------------------------
 
     def file_exists(self, filename):
-        text = filename.encode('utf-8')
-        self.add(32, text)
+        r0 = filename.encode('utf-8')
+        self.add(32, r0)
 
     def file_upload(self, filename, data):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = filename.encode('utf-8')
         r1 = data
-        self.add(33, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(33, _pack_params_struct(r0, r1))
 
-    # UI Map ------------------------------------------------------------------
+    def file_delete(self, filename):
+        r0 = filename.encode('utf-8')
+        self.add(34, r0)
+
+
+    #--------------------------------------------------------------------------
+    # Panel IO Map
+    #--------------------------------------------------------------------------
 
     def panel_create(self, name, local_position):
-        f = '<II'
-        rb = struct.calcsize(f) 
         r0 = name.encode('utf-8')
-        r1 = struct.pack('<fff', local_position[0], local_position[1], local_position[2])
-        self.add(48, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        r1 = struct.pack('<fff', local_position[0], local_position[1], local_position[2])        
+        self.add(48, _pack_params_struct(r0, r1))
 
     def panel_exists(self, name):
         r0 = name.encode('utf-8')
@@ -148,183 +169,157 @@ class command_buffer(hl2ss.umq_command_buffer):
         self.add(50, r0)
 
     def panel_set_active(self, name, active):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = name.encode('utf-8')
         r1 = struct.pack('<I', 1 if (active) else 0)
-        self.add(51, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(51, _pack_params_struct(r0, r1))
 
     def panel_set_transform(self, name, pin_position, panel_positon, panel_scale):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = name.encode('utf-8')
         r1 = struct.pack('<fffffffff', pin_position[0], pin_position[1], pin_position[2], panel_positon[0], panel_positon[1], panel_positon[2], panel_scale[0], panel_scale[1], panel_scale[2])
-        self.add(52, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(52, _pack_params_struct(r0, r1))
+
+
+    #--------------------------------------------------------------------------
+    # Surface IO Map
+    #--------------------------------------------------------------------------
 
     def surface_create(self, parent, name):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
-        self.add(64, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(64, _pack_params_struct(r0, r1))
 
     def surface_exists(self, parent, name):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
-        self.add(65, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(65, _pack_params_struct(r0, r1))
 
     def surface_destroy(self, parent, name):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
-        self.add(66, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(66, _pack_params_struct(r0, r1))
 
     def surface_set_active(self, parent, name, active):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = struct.pack('<I', 1 if (active) else 0)
-        self.add(67, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(67, _pack_params_struct(r0, r1, r2))
 
     def surface_set_transform(self, parent, name, position, rotation, scale):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = struct.pack('<ffffffffff', position[0], position[1], position[2], rotation[0], rotation[1], rotation[2], rotation[3], scale[0], scale[1], scale[2])
-        self.add(68, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(68, _pack_params_struct(r0, r1, r2))
 
     def surface_set_texture(self, parent, name, file_name):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = file_name.encode('utf-8')
-        self.add(69, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(69, _pack_params_struct(r0, r1, r2))
 
     def surface_set_video(self, parent, name, file_name, loop):
-        f = "<IIII"
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = file_name.encode('utf-8')
         r3 = struct.pack('<I', 1 if (loop) else 0)
-        self.add(70, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1), rb + len(r0) + len(r1) + len(r2)) + r0 + r1 + r2 + r3)
+        self.add(70, _pack_params_struct(r0, r1, r2, r3))
+
+
+    #--------------------------------------------------------------------------
+    # Text IO Map
+    #--------------------------------------------------------------------------
 
     def text_create(self, parent, name):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
-        self.add(80, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(80, _pack_params_struct(r0, r1))
         
     def text_exists(self, parent, name):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
-        self.add(81, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(81, _pack_params_struct(r0, r1))
 
     def text_destroy(self, parent, name):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
-        self.add(82, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(82, _pack_params_struct(r0, r1))
 
     def text_set_active(self, parent, name, active):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = struct.pack('<I', 1 if (active) else 0)
-        self.add(83, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(83, _pack_params_struct(r0, r1, r2))
 
     def text_set_transform(self, parent, name, position, dimensions):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = struct.pack('<fffff', position[0], position[1], position[2], dimensions[0], dimensions[1])
-        self.add(84, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(84, _pack_params_struct(r0, r1, r2))
 
     def text_set_format(self, parent, name, font_style, font_size, enable_auto_sizing, rgba_color, horizontal_alignment, vertical_alignment, enable_word_wrap, overflow_mode):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = struct.pack('<IfIffffIIII', font_style, font_size, 1 if (enable_auto_sizing) else 0, rgba_color[0], rgba_color[1], rgba_color[2], rgba_color[3], horizontal_alignment, vertical_alignment, 1 if (enable_word_wrap) else 0, overflow_mode)
-        self.add(85, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(85, _pack_params_struct(r0, r1, r2))
 
     def text_set_text(self, parent, name, text):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = text.encode('utf-8')
-        self.add(86, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(86, _pack_params_struct(r0, r1, r2))
+
+
+    #--------------------------------------------------------------------------
+    # Button IO Map
+    #--------------------------------------------------------------------------
 
     def button_create(self, parent, name, index):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = struct.pack('<I', index)
-        self.add(96, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(96, _pack_params_struct(r0, r1, r2))
 
     def button_exists(self, parent, name):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
-        self.add(97, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(97, _pack_params_struct(r0, r1))
 
     def button_destroy(self, parent, name):
-        f = '<II'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
-        self.add(98, struct.pack(f, rb, rb + len(r0)) + r0 + r1)
+        self.add(98, _pack_params_struct(r0, r1))
 
     def button_set_active(self, parent, name, active):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = struct.pack('<I', 1 if (active) else 0)
-        self.add(99, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(99, _pack_params_struct(r0, r1, r2))
 
     def button_set_transform(self, parent, name, position, rotation, scale):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = struct.pack('<ffffffffff', position[0], position[1], position[2], rotation[0], rotation[1], rotation[2], rotation[3], scale[0], scale[1], scale[2])
-        self.add(100, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(100, _pack_params_struct(r0, r1, r2))
 
     def button_set_text(self, parent, name, text):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = parent.encode('utf-8')
         r1 = name.encode('utf-8')
         r2 = text.encode('utf-8')
-        self.add(101, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(101, _pack_params_struct(r0, r1, r2))
 
     def button_get_state(self, parent):
         r0 = parent.encode('utf-8')
         self.add(102, r0)
 
+
+    #--------------------------------------------------------------------------
+    # Audio Output IO Map
+    #--------------------------------------------------------------------------
+
     def play_wav(self, name, clear, channels, sample_rate, data):
-        f = '<III'
-        rb = struct.calcsize(f)
         r0 = name.encode('utf-8')
         r1 = struct.pack('<III', channels, sample_rate, 1 if (clear) else 0)
         r2 = data
-        self.add(112, struct.pack(f, rb, rb + len(r0), rb + len(r0) + len(r1)) + r0 + r1 + r2)
+        self.add(112, _pack_params_struct(r0, r1, r2))
 
     def is_wav_playing(self):
         self.add(113, b'')
