caa6d24b38914d3b3f1da6923c8f6cb9ad2f018e
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-07-18 03:34:09 -0400

    Update ButtonEvent.cs

--- uifm/Assets/Scripts/ButtonEvent.cs
+++ uifm/Assets/Scripts/ButtonEvent.cs
@@ -1,13 +1,9 @@
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Microsoft.MixedReality.Toolkit.Audio;
 
 public class ButtonEvent : MonoBehaviour
 {
     public bool pressed = false;
-    public int index = 0;
 
     // Start is called before the first frame update
     void Start()
