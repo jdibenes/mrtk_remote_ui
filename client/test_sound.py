70bba39df48074cbad5a010eaaea2899b7e283b4
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-07-18 05:18:40 -0400

    Update test_sound.py

--- client/test_sound.py
+++ client/test_sound.py
@@ -38,26 +38,26 @@ listener.start()
 client_outbound = hl2ss_lnm.ipc_umq(host, hl2ss.IPCPort.UNITY_MESSAGE_QUEUE)
 client_outbound.open()
 
-cmdbuf = hl2ss_uifm.command_buffer()
-
 for audio_file_descriptor in audio_file_descriptors:
     file_name = audio_file_descriptor[0]
     audio_type = audio_file_descriptor[1]
 
+    print(f'Uploading file {file_name} to HoloLens...')
+
     with open(os.path.join(data_folder, file_name), 'rb') as audio_file:
+        cmdbuf = hl2ss_uifm.command_buffer()
         cmdbuf.file_upload(file_name, audio_file.read())
 
-client_outbound.push(cmdbuf)
-response = client_outbound.pull(cmdbuf)
+        client_outbound.push(cmdbuf)
+        response = client_outbound.pull(cmdbuf)
 
-# For server responses:
-#     response <  0x80000000 : OK
-#     response >= 0x80000000 : ERROR
-print(f'file_upload response: {response}')
+        print(f'file_upload response: {response[0]}')
 
 audio_index = 0
 
 while (enable):
+    print('Playing next audio file...')
+
     audio_file_descriptor = audio_file_descriptors[audio_index]
 
     file_name = audio_file_descriptor[0]
@@ -81,6 +81,8 @@ while (enable):
         response = client_outbound.pull(cmdbuf)
 
         if (response[0] == 0):
+            print('Audio file playback complete')
+
             audio_index = (audio_index + 1) % len(audio_file_descriptors)
             break
 
@@ -89,5 +91,7 @@ cmdbuf.audio_control(hl2ss_uifm.AudioOperation.STOP)
 
 client_outbound.push(cmdbuf)
 client_outbound.pull(cmdbuf)
+
+print('Audio stopped')
 
 client_outbound.close()
